a1240adb86578338bb2763e938d77cf08498a49c
Tony Jiang <tony.jiang.zhengping@gmail.com>
2016-12-10 00:15:20 +0800

    get along with file operation.

---
+++ clustering.py
@@ -0,0 +1,12 @@
+import numpy as np
+# import matplotlib.pyplot as plt
+
+# from sklearn import cluster, datasets
+# from sklearn.neighbors import neighbors_graph
+# from sklearn.preprocessing import StandardScaler
+
+with open("../input.txt") as f:
+    for line in f:
+        print line
+
+np.random.seed(0)
